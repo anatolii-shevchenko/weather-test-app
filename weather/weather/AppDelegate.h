43afd93a1f99966ef63e62c71c73c76c1e1b8b70
Tolik Shevchenko <anatolii.shevchenko@p-product.com>
2015-02-28 09:43:25 +0200

    Template project has been created.

---
+++ weather/weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather
+//
+//  Created by Tolik on 2/28/15.
+//  Copyright (c) 2015 Tolik Shevchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weather/weather/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  weather
+//
+//  Created by Tolik on 2/28/15.
+//  Copyright (c) 2015 Tolik Shevchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
